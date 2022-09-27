dd6397e7f752177933d495607fb60414d0c974bf
JungBear <mod459@naver.com>
2022-09-27 16:33:46 +0900

    updated

---
+++ source/day09/day0927/0927 다형성 제네릭 컬렉션프레임워크/자바중급1/src/generic/Cat.java
@@ -0,0 +1,18 @@
+package generic;
+
+/**
+ * 
+ * @author 배성원
+ * @param 파라미터 없음
+ * @apiNote 고양이 객체
+ *
+ */
+public class Cat {
+	int age;
+	double weight;
+	double height;
+	
+	public void cry() {
+		 System.out.println("야오옹");
+	}
+}
